bedab75e02e630f395e1f5dff3b876b7ef992f05
Sakshimishra4745 <mmishra.sakshi20@gmail.com>
2025-08-25 14:47:48 +0530

    usermodel

---
+++ models/usermodel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const customerschema = new mongoose.Schema({
+    userName:{
+        type:String,
+        require:true,
+    },
+    email:{
+      type:String,  
+    require:true,
+    },
+    password:{
+        type:String,
+        require:true,
+
+    }
+})
+
+const user = mongoose.model("customer", customerschema);
+
+export default user;
